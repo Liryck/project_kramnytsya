07ad10d93b919f772e95c3b228a8772eb500be2c
Liryck <liryckbragin@gmail.com>
2022-06-27 22:09:35 +0300

    add script, fixed bugs, add comment with carousel

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,13 @@ function toggleSlide(item) {
 
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
+
+//Carousel
+// $(document).ready(function(){
+//     $('.catalog-item__slide').slick({
+//         speed: 1200,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+//     });
+//   });
